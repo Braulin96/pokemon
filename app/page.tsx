6f622fb3fc1b03df0760265be7d1febbf1fdd7db
Braulin Pires <braulinpires@hotmail.com>
2024-03-07 10:05:22 +0000

    removed files not used and added a welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 export default function Home() {
   return (
-   <main>
-    <div className="bg-red-500">
-    <p className="text-green-400 text-xl"> Testing Tailwind</p>
+    <div
+      className="flex h-full my-auto bg-red-400 justify-center"
+      style={{
+        height: "100vh",
+        maxHeight: "1000px",
+      }}
+    >
+      <div className="max-w-7xl bg-blue-500 my-auto text-center">
+        <p className="text-3xl font-extralight">
+          Welcome to our PokeApp <br /> Let's find your favorite pokemon
+        </p>
+
+      </div>
     </div>
-   </main>
   );
 }
